adaa1d821594c6b61f8ad66d131f7ddd83693f0e
0xEstarriol <Estarriol.Vetch@protonmail.com>
2023-02-16 21:21:14 -0500

    deployment script

--- deploy/01_factory.js
+++ deploy/01_factory.js
@@ -17,7 +17,7 @@ module.exports = async ({
   console.log('deployer: ', deployer.address);
   console.log('=============================');
 
-
+/**
   let ClaimableNFT = await deploy('ClaimableNFT', {
     from: deployer.address,
     gasLimit: 4000000,
@@ -39,6 +39,43 @@ module.exports = async ({
     await hre.tenderly.verify({
       name: 'ClaimableNFT',
       address: ClaimableNFT.implementation,
+    });
+  } */
+
+  let Claimable1155 = await deploy('Claimable1155', {
+    from: deployer.address,
+    gasLimit: 4000000,
+    args: [],
+  });
+
+  console.log('Claimable1155 address: ', Claimable1155.address);
+
+  let Factory = await deploy('Factory', {
+    from: deployer.address,
+    gasLimit: 4000000,
+    proxy: {
+      execute: {
+        init: {
+          methodName: 'initialize',
+          args: [Claimable1155.address],
+        },
+      },
+      proxyContract: 'OpenZeppelinTransparentProxy',
+    },
+    args: [],
+  });
+
+  console.log('Factory address: ', Factory.address);
+
+  if (networkName !== 'hardhat') {
+    await hre.tenderly.verify({
+      name: 'Claimable1155',
+      address: Claimable1155.address,
+    });
+
+    await hre.tenderly.verify({
+      name: 'Factory',
+      address: Factory.implementation,
     });
   }
 
